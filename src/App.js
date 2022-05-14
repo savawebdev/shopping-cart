5cf39622b929853270521678deecb5a77cb488ec
savawedev <andrei.sava.dev@gmail.com>
2022-05-14 16:08:24 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import Header from "./components/Header/Header";
+import Home from "./components/Pages/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Store from "./components/Pages/Store";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
       <NavBar />
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </Fragment>
   );
 };
