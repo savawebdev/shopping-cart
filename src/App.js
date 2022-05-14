aefc24ab7372ed213f643a36b4fc1ee6e11d556c
savawedev <andrei.sava.dev@gmail.com>
2022-05-14 14:39:30 +0300

    Added NavBar base component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import styles from "./App.module.css";
 import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <Fragment>
       <Header />
+      <NavBar />
     </Fragment>
   );
 };
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return <nav className={styles.navbar}>Link</nav>;
+};
+
+export default NavBar;
