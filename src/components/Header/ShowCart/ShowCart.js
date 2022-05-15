6feb42da4a81ee34653208169e606e6db0b232b1
savawedev <andrei.sava.dev@gmail.com>
2022-05-15 07:39:28 +0300

    Styled cart component

--- src/components/Header/ShowCart/ShowCart.js
+++ src/components/Header/ShowCart/ShowCart.js
@@ -1,21 +1,30 @@
 import styles from "./ShowCart.module.css";
 
 const ShowCart = ({ cart }) => {
+  const totalPrice = cart.items.reduce(
+    (sum, cur) => sum + cur.price * cur.quantity,
+    0
+  );
   return (
     <div className={styles["show-cart"]}>
       {cart.items.length === 0 && <span>There are no items in the cart.</span>}
 
       {cart.items.map((item) => (
         <div className={styles["cart-item"]}>
-          <span>{item.name}</span>
-          <span>x{item.quantity}</span>
-          <span>{item.price * item.quantity} &euro;</span>
+          <span className={styles["cart-item__name"]}>{item.name}</span>
+          <span className={styles["cart-item__quantity"]}>
+            x{item.quantity}
+          </span>
+          <span className={styles["cart-item__price"]}>
+            {item.price * item.quantity} &euro;
+          </span>
         </div>
       ))}
 
       {cart.items.length !== 0 && (
         <div className={styles["cart-total"]}>
-          {cart.items.reduce((sum, cur) => sum + cur.price * cur.quantity, 0)}
+          Total Price:
+          <span> {totalPrice} &euro;</span>
         </div>
       )}
     </div>
